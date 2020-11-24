0e0e308ffc32e44eea60e2520749020830de1de7
znv-nataly <znv.nataly@gmail.com>
2020-11-24 15:06:58 +0500

    After lecture

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -5,11 +5,11 @@ import android.content.Context
 import android.graphics.Rect
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 fun Activity.hideKeyboard() {
-    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(et_message.windowToken, 0)
+//    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//    imm.hideSoftInputFromWindow(et_message.windowToken, 0)
 }
 
 fun Activity.isKeyboardClosed(): Boolean {
--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,33 +1,35 @@
-package ru.skillbranch.devintensive
+package ru.skillbranch.devintensive.ui.profile
 
-
-import android.graphics.Color
+import android.graphics.ColorFilter
 import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
-import ru.skillbranch.devintensive.extensions.hideKeyboard
-import ru.skillbranch.devintensive.extensions.isKeyboardClosed
-import ru.skillbranch.devintensive.models.Bender
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_profile_constraint.*
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 
-const val SAVE_STATE_STATUS = "STATUS"
-const val SAVE_STATE_QUESTION = "QUESTION"
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+//        const val SAVE_STATE_STATUS = "STATUS"
+//        const val SAVE_STATE_QUESTION = "QUESTION"
+        private const val IS_EDIT_MODE = "IS_EDIT_MODE"
 
-    lateinit var benderImage: ImageView
-    lateinit var textTxt: TextView
-    lateinit var messageEt: EditText
-    lateinit var sendBtn: ImageView
+    }
 
-    lateinit var benderObj: Bender
+    private lateinit var viewModel: ProfileViewModel
+    var isEditMode = false
+
+    lateinit var viewFields: Map<String, TextView>
 
 
     /**
@@ -42,38 +44,87 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
      * Всегда сопровождается вызовом onStart().
      */
     override fun onCreate(savedInstanceState: Bundle?) {
+        // TODO set custom theme this before super and setContentView
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile_constraint)
+        initViews(savedInstanceState)
+        initViewModel()
+        Log.d("M_ProfileActivity", "onCreate")
+    }
 
-//        benderImage = findViewById(R.id.iv_bender) as ImageView
-        benderImage = iv_bender
-        textTxt = tv_text
-        messageEt = et_message
-        sendBtn = iv_send
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel.getProfileDate().observe(this, Observer { updateUI(it) })
+        viewModel.getTheme().observe(this, Observer { updateTheme(it) })
+    }
 
-        val status = savedInstanceState?.getString(SAVE_STATE_STATUS) ?: Bender.Status.NORMAL.name
-        val question = savedInstanceState?.getString(SAVE_STATE_QUESTION) ?: Bender.Question.NAME.name
-        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+    private fun updateTheme(theme: Int) {
+        Log.d("M_ProfileActivity", "updateTheme $theme")
+        delegate.localNightMode = theme // activity будет пересоздана
+    }
 
-        Log.d("M_MainActivity", "onCreate $status $question")
-
-        val (r, g, b) = benderObj.status.color
-        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
-
-        textTxt.text = benderObj.askQuestion()
-        sendBtn.setOnClickListener(this)
-
-        et_message.setOnEditorActionListener() { view, actionId, keyEvent ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                sendBtn.callOnClick()
-                hideKeyboard()
-                true
-            } else {
-                false
+    private fun updateUI(profile: Profile) {
+        profile.toMap().also {
+            for ((k, v) in viewFields) {
+                v.text = it[k].toString()
             }
         }
     }
 
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields = mapOf(
+            "nickName" to tv_nick_name,
+            "rank" to tv_rank,
+            "firstName" to et_first_name,
+            "lastName" to et_last_name,
+            "about" to et_about,
+            "repository" to et_repository,
+            "rating" to tv_rating,
+            "respect" to tv_respect
+        )
+        isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+        showCurrentMode(isEditMode)
+
+        btn_edit.setOnClickListener {
+            if (isEditMode) saveProfileInfo()
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+        }
+
+        btn_switch_theme.setOnClickListener {
+            viewModel.switchTheme()
+        }
+    }
+
+    private fun showCurrentMode(isEdit: Boolean) {
+        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        for ((_, v) in info) {
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if (isEdit) 255 else 0
+        }
+        ic_eye.visibility = if (isEdit) View.GONE else View.VISIBLE
+        wr_about.isCounterEnabled = isEdit
+
+        with(btn_edit) {
+            val filter: ColorFilter? = if (isEdit) {
+                PorterDuffColorFilter(resources.getColor(R.color.color_accent, theme), PorterDuff.Mode.SRC_IN)
+            } else {
+                null
+            }
+
+            val icon = if (isEdit) {
+                resources.getDrawable(R.drawable.ic_save_black_24dp, theme)
+            } else {
+                resources.getDrawable(R.drawable.ic_edit_black_24dp, theme)
+            }
+
+            background.colorFilter = filter
+            setImageDrawable(icon)
+        }
+    }
 
     /**
      * Если Activity возвращается в приоритетный режим после вызова onStop(), то в этом случае вызывается метод onRestart().
@@ -165,21 +216,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Log.d("M_MainActivity", "onDestroy")
     }
 
-    override fun onClick(view: View?) {
-        if (view?.id == R.id.iv_send) {
-            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
-            messageEt.setText("")
-            val (r, g, b) = color
-            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
-            textTxt.text = phrase
-        }
-    }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        outState.putBoolean(IS_EDIT_MODE, isEditMode)
 
-        outState.putString(SAVE_STATE_STATUS, benderObj.status.name)
-        outState.putString(SAVE_STATE_QUESTION, benderObj.question.name)
-        Log.d("M_MainActivity", "onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
+//        outState.putString(SAVE_STATE_STATUS, benderObj.status.name)
+//        outState.putString(SAVE_STATE_QUESTION, benderObj.question.name)
+//        Log.d("M_MainActivity", "onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
+    }
+
+    private fun saveProfileInfo() {
+        val firstName = et_first_name.text.toString()
+        val lastName = et_last_name.text.toString()
+        Profile(
+            firstName = firstName,
+            lastName = lastName,
+            about = et_about.text.toString(),
+            repository = et_repository.text.toString()
+        ).apply {
+            viewModel.saveProfileDate(this)
+        }
     }
 }
